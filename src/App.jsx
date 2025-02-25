0c6a58a92bf385279b477bc225ae73222c03eca5
pearry22 <pearry.200228@gmail.com>
2025-02-25 10:27:51 +0700

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,26 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
+import Contact from "./assets/Contact";
+import Counter from "./assets/Counter";
 import Hello from "./assets/Hello";
 
 function App() {
+  const helloData= [
+    {name: 'Pearry', message:'Hi...'},
+    {name: 'Ohm', message:'Hi...'}
+  ];
+
   return (
-    <div className='App'>
-      <Hello />
-    </div>
+    <div className="App">
+      <Counter/>
+      {helloData.map((data,index)=>(
+        <Hello key={index} name={data.name} message={data.message}/>
+      ))}
+
+      < Contact email= "pearry.200228@gamil.com" phone="0983967928" />
+    </div> 
   );
-}
+    }
 
 export default App
--- src/assets/Hello.jsx
+++ src/assets/Hello.jsx
@@ -1,8 +1,19 @@
-function Hello(){
+const Hello = ({ message,name}) =>{
+    console.log({message,name});
     return (
-        <div>
-            <h1> Hello Pearry !!!!! </h1>
-        </div>
-    )
-}
-export default Hello;
+    <div>
+        <h1>
+            {message} {name}
+        </h1>
+    </div>
+    );
+};
+
+import PropTypes from 'prop-types';
+
+Hello.propTypes = {
+      email: PropTypes.string.isRequired,
+        phone: PropTypes.string.isRequired,
+    };
+
+    export default Hello;
