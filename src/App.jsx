8698f86403f04e76f60ccd50ba39d3a9d48676ba
pearry22 <pearry.200228@gmail.com>
2025-02-25 11:06:10 +0700

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,119 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-import Contact from "./assets/Contact";
-import Counter from "./assets/Counter";
-import Hello from "./assets/Hello";
+function ProductCategoryRow({ category }) {
+  return (
+    <tr>
+      <th colSpan="2">
+        {category}
+      </th>
+    </tr>
+  );
+}
 
-function App() {
-  const helloData= [
-    {name: 'Pearry', message:'Hi...'},
-    {name: 'Ohm', message:'Hi...'}
-  ];
+function ProductRow({ product }) {
+  const name = product.stocked ? product.name :
+    <span style={{ color: 'red' }}>
+      {product.name}
+    </span>;
 
   return (
-    <div className="App">
-      <Counter/>
-      {helloData.map((data,index)=>(
-        <Hello key={index} name={data.name} message={data.message}/>
-      ))}
-
-      < Contact email= "pearry.200228@gamil.com" phone="0983967928" />
-    </div> 
+    <tr>
+      <td>{name}</td>
+      <td>{product.price}</td>
+    </tr>
   );
-    }
+}
 
-export default App
+function ProductTable({ products }) {
+  const rows = [];
+  let lastCategory = null;
+
+  products.forEach((product) => {
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow
+          category={product.category}
+          key={product.category} />
+      );
+    }
+    rows.push(
+      <ProductRow
+        product={product}
+        key={product.name} />
+    );
+    lastCategory = product.category;
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
+
+function SearchBar() {
+  return (
+    <form>
+      <input type="text" placeholder="Search dodi..." />
+      <label>
+        <input type="checkbox" />
+        {' '}
+        Only show products in stock wow!!!
+      </label>
+    </form>
+  );
+}
+
+function FilterableProductTable({ products }) {
+  return (
+    <div>
+      <SearchBar />
+      <ProductTable products={products} />
+    </div>
+  );
+}
+
+const PRODUCTS = [
+  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+];
+
+export default function App() {
+  return <FilterableProductTable products={PRODUCTS} />;
+}
+
+// // import { useState } from 'react'
+// // import reactLogo from './assets/react.svg'
+// // import viteLogo from '/vite.svg'
+// // import './App.css'
+// import Contact from "./assets/Contact";
+// import Counter from "./assets/Counter";
+// import Hello from "./assets/Hello";
+
+// function App() {
+//   const helloData= [
+//     {name: 'Pearry', message:'Hi...'},
+//     {name: 'Ohm', message:'Hi...'}
+//   ];
+
+//   return (
+//     <div className="App">
+//       <Counter/>
+//       {helloData.map((data,index)=>(
+//         <Hello key={index} name={data.name} message={data.message}/>
+//       ))}
+
+//       < Contact email= "pearry.200228@gamil.com" phone="0983967928" />
+//     </div> 
+//   );
+//     }
+
+// export default App
